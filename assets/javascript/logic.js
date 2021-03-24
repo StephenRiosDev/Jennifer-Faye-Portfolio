78152ef0ab565111c51030d998aceebe831082c6
Jennifer Faye <jennifer@fayecreative.com>
2021-03-23 22:38:28 -0500

    navbar scrolls up, then becomes sticky

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,5 +3,22 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
-  });
-      
+
+// execute navFunction on scroll
+window.onscroll = function(){navFunction()};
+
+// get navbar
+const navBar = document.getElementById("contents-wrapper");
+  
+// get offset position of navbar
+const sticky = navBar.offsetTop;
+
+// reach scroll position - add sticky, leave scroll position - remove sticky
+function navFunction() {
+  if (window.pageYOffset >= sticky) {
+    navBar.classList.add("sticky")
+  } else {
+    navBar.classList.remove("sticky");
+  }
+}
+});
