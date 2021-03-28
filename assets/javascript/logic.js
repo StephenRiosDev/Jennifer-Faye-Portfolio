922dc295e1699f61794bbb30a682c71d4c286abc
Jennifer Faye <jennifer@fayecreative.com>
2021-03-28 14:50:57 -0500

    making responsive before adding media queries

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,22 +4,37 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
+    //  $('.pushpin').pushpin();
 
-// execute navFunction on scroll
-window.onscroll = function(){navFunction()};
 
-// get navbar
-const navBar = document.getElementById("contents-wrapper");
+
   
-// get offset position of navbar
-const sticky = navBar.offsetTop;
+        // execute navFunction on scroll
+        window.onscroll = function(){navFunction()};
+        // get navbar
+        const navBar = document.getElementById("contents-wrapper");
+        const sideNav = document.getElementById("sidebar-wrapper");
+        // get offset position of navbar
+        const sticky = navBar.offsetTop;
+        const sideStick = sideNav.offsetTop;
 
-// reach scroll position - add sticky, leave scroll position - remove sticky
-function navFunction() {
-  if (window.pageYOffset >= sticky) {
-    navBar.classList.add("sticky")
-  } else {
-    navBar.classList.remove("sticky");
-  }
-}
+        // reach scroll position - add sticky, leave scroll position - remove sticky
+        function navFunction() {
+          if (window.pageYOffset >= sticky) {
+            navBar.classList.add("sticky")
+          } else {
+            navBar.classList.remove("sticky");
+          }
+          if (window.pageYOffset >= sideStick) {
+            sideNav.classList.add("sideStick")
+          } else {
+            sideNav.classList.remove("sideStick");
+          }
+        }
+
+  
+
+
+
+
 });
