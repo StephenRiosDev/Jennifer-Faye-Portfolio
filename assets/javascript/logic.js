27b3cf7dade92ada129c8e2a26ca2b5de60c4e50
Jennifer Faye <jennifer@fayecreative.com>
2021-07-21 11:22:54 -0500

    moved about section down

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,44 +4,35 @@ $(document).ready(function(){
     $('.parallax').parallax();
    
     $('.tooltipped').tooltip();
-     $('.pushpin').pushpin();
+    $('.pushpin').pushpin();
 
-     $('.sidenav').sidenav({
-       menuWidth: 300,
-       edge: 'left',
-       closeOnClick: true,
-       draggable: true
-     });
-      
+    $('.sidenav').sidenav({
+      menuWidth: 300,
+      edge: 'left',
+      closeOnClick: true,
+      draggable: true
+    });
 
+    // execute navFunction on scroll
+    window.onscroll = function(){navFunction()};
+    // get navbar
+    const navBar = document.getElementById("contents-wrapper");
+    const sideNav = document.getElementById("sidebar-wrapper");
+    // get offset position of navbar
+    const sticky = navBar.offsetTop;
+    const sideStick = sideNav.offsetTop;
 
-  
-        // execute navFunction on scroll
-        window.onscroll = function(){navFunction()};
-        // get navbar
-        const navBar = document.getElementById("contents-wrapper");
-        const sideNav = document.getElementById("sidebar-wrapper");
-        // get offset position of navbar
-        const sticky = navBar.offsetTop;
-        const sideStick = sideNav.offsetTop;
-
-        // reach scroll position - add sticky, leave scroll position - remove sticky
-        function navFunction() {
-          if (window.pageYOffset >= sticky) {
-            navBar.classList.add("sticky")
-          } else {
-            navBar.classList.remove("sticky");
-          }
-          if (window.pageYOffset >= sideStick) {
-            sideNav.classList.add("sideStick")
-          } else {
-            sideNav.classList.remove("sideStick");
-          }
-        }
-
-  
-
-
-
-
+    // reach scroll position - add sticky, leave scroll position - remove sticky
+    function navFunction() {
+      if (window.pageYOffset >= sticky) {
+        navBar.classList.add("sticky")
+      } else {
+        navBar.classList.remove("sticky");
+      }
+      if (window.pageYOffset >= sideStick) {
+        sideNav.classList.add("sideStick")
+      } else {
+        sideNav.classList.remove("sideStick");
+      }
+    }
 });
