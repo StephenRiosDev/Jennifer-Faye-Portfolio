ea0ffb5d0c4f18823f1deedc5ad771b49dd5bc91
Jennifer Faye <jennifer@fayecreative.com>
2024-02-07 21:28:08 -0600

    sidenav on redo page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     $('.sidenav').sidenav({
       menuWidth: 300,
-      edge: 'right',
+      // edge: 'right',
       closeOnClick: true,
       draggable: true
     });
